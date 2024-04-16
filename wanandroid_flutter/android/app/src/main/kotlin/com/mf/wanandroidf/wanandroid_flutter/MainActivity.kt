aaf67b35381a0adea722e9a2523a08f603aa57c8
MuFeng <hanlin_bj@163.com>
2024-04-16 11:37:52 +0800

    项目创建

---
+++ wanandroid_flutter/android/app/src/main/kotlin/com/mf/wanandroidf/wanandroid_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mf.wanandroidf.wanandroid_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ wanandroid_swift/wanandroid_swift/wanandroid_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  wanandroid_swiftApp.swift
+//  wanandroid_swift
+//
+//  Created by MuFeng on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct wanandroid_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
